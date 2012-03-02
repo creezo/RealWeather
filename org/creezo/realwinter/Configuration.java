7711b356afd824c2b868b889d5bab2e0134dc269
Daniel Svabek <Dodec@.(none)>
2012-03-02 12:25:02 +0100

    Added StartDelay config.

--- org/creezo/realwinter/Configuration.java
+++ org/creezo/realwinter/Configuration.java
@@ -19,4 +19,9 @@ public class Configuration {
         enabled = state;
         return enabled;
     }
+    
+    public int StartDelay() {
+        int StartDelay = 20;
+        return StartDelay;
+    }
 }
